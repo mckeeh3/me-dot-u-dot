e4df2bdd7bcc6142651256e7f18e6a8f6775a39f
mckeeh3 <mckeeh3@gmail.com>
2025-08-27 07:02:40 -0400

    fix AI model config settings

--- src/main/java/com/example/Bootstrap.java
+++ src/main/java/com/example/Bootstrap.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigValueType;
 
 import akka.javasdk.ServiceSetup;
 import akka.javasdk.annotations.Setup;
@@ -19,17 +20,36 @@ public class Bootstrap implements ServiceSetup {
           "No API keys found. Make sure you have OPENAI_API_KEY defined as environment variable, or change the model provider configuration in application.conf to use a different LLM.");
     }
 
-    log.info("================================================================================");
-    log.info("Model providers: akka.javasdk.agent");
-    log.info("--------------------------------------------------------------------------------");
-    config.getObject("akka.javasdk.agent").unwrapped().forEach((key, value) -> {
-      log.info("Model provider: {}: {}", key, value);
-    });
+    config.getObject("akka.javasdk.agent").entrySet()
+        .forEach(entry -> {
+          var value = entry.getValue();
+
+          if (value.valueType() == ConfigValueType.OBJECT) {
+            log.info("================================================================================");
+            log.info("Reference configuration: {} ({})", entry.getKey(), value.valueType().name());
+            log.info("--------------------------------------------------------------------------------");
+
+            var modelConfig = config.getConfig("akka.javasdk.agent." + entry.getKey());
+            modelConfig.entrySet()
+                .forEach(field -> {
+                  if ("api-key".equals(field.getKey())) {
+                    log.info("Field: {}: {}", field.getKey(), "********");
+                  } else {
+                    log.info("Field: {}: {}", field.getKey(), field.getValue().unwrapped().toString());
+                  }
+                });
+          } else {
+            var configValueType = value.valueType().name();
+            log.info("================================================================================");
+            log.info("Reference configuration: {} = {} ({})", entry.getKey(), value.unwrapped().toString(), configValueType);
+            log.info("--------------------------------------------------------------------------------");
+          }
+        });
 
     config.root().entrySet().stream().filter(entry -> entry.getKey().startsWith("ai-agent-model-"))
         .forEach(entry -> {
           log.info("================================================================================");
-          log.info("Entry: {}", entry.getKey());
+          log.info("Dot Game Agent model configuration: {}", entry.getKey());
           log.info("--------------------------------------------------------------------------------");
 
           var modelConfigName = entry.getKey().substring("ai.agent.model.".length());
@@ -37,10 +57,10 @@ public class Bootstrap implements ServiceSetup {
 
           var modelConfig = config.getConfig(entry.getKey());
           modelConfig.entrySet().forEach(modelEntry -> {
-            if (!"api-key".equals(modelEntry.getKey())) {
-              log.info("Model entry: {}: {}", modelEntry.getKey(), modelEntry.getValue().unwrapped().toString());
-            } else {
+            if ("api-key".equals(modelEntry.getKey())) {
               log.info("Model entry: {}: {}", modelEntry.getKey(), "********");
+            } else {
+              log.info("Model entry: {}: {}", modelEntry.getKey(), modelEntry.getValue().unwrapped().toString());
             }
           });
         });
